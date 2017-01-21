18e3fe4e9fabe2e14705e2e51a8cb823217e0754
Colin Wright <cdotwright@gmail.com>
2017-01-21 14:31:53 -0800

    physics fps controller

---
+++ Assets/playerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour {
+
+
+	public float speed;
+	public GvrHead head;
+	private Rigidbody rb;
+
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>(); 
+		rb.position = transform.position;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (head == null) {
+			//unity sucks
+			head = Camera.main.GetComponent<StereoController> ().Head;
+		}
+		rb.position += speed * head.Gaze.direction;
+	}
+}
