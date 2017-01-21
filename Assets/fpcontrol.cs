ce0d09adae0faca91928220aae9536b183e8c3a2
Colin Wright <cdotwright@gmail.com>
2017-01-20 22:56:29 -0800

    first person control

---
+++ Assets/fpcontrol.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fpcontrol : MonoBehaviour {
+	public float speed;
+	public GvrHead head;
+	// Use this for initialization
+	void Start () {
+		
+		head = Camera.main.GetComponent<StereoController>().Head;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (head == null) {
+			head = Camera.main.GetComponent<StereoController> ().Head;
+		}
+		transform.position += speed * head.Gaze.direction;
+	}
+}
